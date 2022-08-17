b615bdf61a1f7d1ad291647f202ecfc39a4ce1ce
Sharon Klinkenberg <s.klinkenberg@uva.nl>
2022-08-17 17:44:02 +0200

    added some comments

--- item_crawler.R
+++ item_crawler.R
@@ -1,4 +1,6 @@
+# retrieve path to all rmarkdown files
 all.item.paths <- list.files(pattern = ".Rmd", ignore.case = TRUE, recursive = TRUE)
+
 n = length(all.item.paths)
 
 for (i in 1:n) {
@@ -15,5 +17,6 @@ for (i in 1:n) {
   
   # replace word in text file using sed in bash
   # sed -i '' -E 's/<string-to-find>/<string-to-replace>/' <your-file-path>
+  # run sed in bash through system()
   
 }
