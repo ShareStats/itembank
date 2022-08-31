3349f159f93d13d8bf766ad163d2ab1c60cf936c
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2022-08-31 23:12:47 +0200

    update scripts & add item-errors

--- scripts/compile_items.R
+++ scripts/compile_items.R
@@ -1,5 +1,5 @@
 # Install development version of exams::
-install.packages("exams", repos = "http://R-Forge.R-project.org", dependencies = TRUE, type = "source")
+#install.packages("exams", repos = "http://R-Forge.R-project.org", dependencies = TRUE, type = "source")
 library(exams)
 
 # retrieve path to all rmarkdown files
@@ -14,24 +14,34 @@ folders <- gsub('/[^/]+$','', all.item.paths)
 # extract the name of the item without extension
 names <- gsub('.*/','', folders)
 
-for (i in 1:n) {
-  # use exam package to compile 
-  # from path i
-  # to path gsub i transform .Rmd to .html
-  # (using the developers veresion)
-  exams:::browse_exercise(
-    # compile each .Rmd file...
-    file = all.item.paths[i],
-    # ...with a given name...
-             name = names[i],
-    # ...to a given folder.
-             dir = folders[i])
-}
+mist <- c()
 
-## NOTE: It runs but stops when Error is given,
-# e.g., wrong exsolution
+for (i in 1:n) {
   
-  # compile to 
+  tryCatch({
+    exams:::browse_exercise(
+      # compile each .Rmd file...
+      file = all.item.paths[i],
+      # ...with a given name...
+      name = names[i],
+      # ...to a given folder.
+      dir = folders[i])
+  }, error = function(e){
+    mist <<- rbind(mist, paste('Item:', names[i],"; Error:",conditionMessage(e)))
+  }
+  )
+}
+mist  
+# use exam package to compile 
+# from path i
+# to path gsub i transform .Rmd to .html
+# (using the developers veresion)
+Item_Errors <- mist
+
+write.table(Item_Errors, file = "Item_Errors.txt", sep = "\n",
+            row.names = FALSE)
+  
+# compile to 
   # html
   # QTI 2.1
   # Testvision
