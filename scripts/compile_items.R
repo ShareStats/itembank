c459a1d78b24647921646e7bddbe2e9c3a8c755a
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2022-09-05 15:18:58 +0200

    QTI 2.1 compile

--- scripts/compile_items.R
+++ scripts/compile_items.R
@@ -2,6 +2,7 @@
 #install.packages("exams", repos = "http://R-Forge.R-project.org", dependencies = TRUE, type = "source")
 library(exams)
 
+# setwd("/Users/tasospsy/Google Drive/ShareStats/itembank/")
 # retrieve path to all rmarkdown files
 all.item.paths <- list.files(pattern = ".Rmd", ignore.case = TRUE, recursive = TRUE)
 
@@ -15,15 +16,24 @@ folders <- gsub('/[^/]+$','', all.item.paths)
 names <- gsub('.*/','', folders)
 
 mist <- c()
-
 for (i in 1:n) {
-  
   tryCatch({
-    exams:::browse_exercise(
+    ## To Compile to .html uncomment the following line
+    # exams:::browse_exercise(
+    
+    ## To Compile to QTI 2.1 uncomment the following line
+    # exams::exams2qti21(
+    
+    ## To Compile to testVision uncomment the following line
+    exams::exams2testvision(
+    
+    ## Run all the rest as it is 
       # compile each .Rmd file...
       file = all.item.paths[i],
+      
       # ...with a given name...
       name = names[i],
+      
       # ...to a given folder.
       dir = folders[i])
   }, error = function(e){
@@ -32,18 +42,16 @@ for (i in 1:n) {
   )
 }
 mist  
-# use exam package to compile 
-# from path i
-# to path gsub i transform .Rmd to .html
-# (using the developers veresion)
-Item_Errors <- mist
 
-write.table(Item_Errors, file = "Item_Errors.txt", sep = "\n",
+Item_Qti_Errors <- mist
+
+write.table(Item_Qti_Errors, file = "Item_Qti_Errors.txt", sep = "\n",
             row.names = FALSE)
   
-# compile to 
-  # html
-  # QTI 2.1
-  # Testvision
-  
 
+## QTI compilation
+## because 'exams2qti21' gives a .zip file as output,
+## we rename everything to <path-file>-qti.zip
+Zips <- list.files(pattern = ".zip", ignore.case = TRUE, recursive = TRUE)
+newZips <- gsub(".zip", "-qti.zip", Zips )
+file.rename(Zips, newZips)
