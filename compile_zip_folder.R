46ae497c57a57c9c5ed4750c1edfb0a93564e7ed
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2022-10-05 14:56:39 +0200

    add compile zip script

---
+++ compile_zip_folder.R
@@ -0,0 +1,38 @@
+# -------------------------------------------------------------------------
+# Compile Zip File 4/10/2022 ----------------------------------------------
+
+# Function to list files --------------------------------------------------
+
+List_All_Files <- function(patt){
+  res <- list.files(pattern = patt, 
+                   ignore.case = TRUE, 
+                   recursive = TRUE)
+  return(res)
+} 
+
+# List the relevant files -------------------------------------------------
+init.dir <- "/Users/tasospsy/Google Drive/ShareStats/itembank"
+setwd(init.dir)
+All.paths <- List_All_Files(".Rmd") # I use .Rmd bc we know it is a unique key
+Folders <- gsub('/[^/]+$','', All.paths)
+
+for (f in Folders){
+  setwd(paste0(init.dir,"/", f))
+  HTML<- List_All_Files(".html") # All html files
+  ZIP <- List_All_Files(".zip")  # All zip files
+  TXT <- List_All_Files(".txt")  # All txt files
+  ALL <- List_All_Files(".") # everything else but folders
+  ##  we do NOT want:
+  OUT <- c(HTML, ZIP, TXT)
+  ##  we do want:
+  IN <- setdiff(x = ALL, y = OUT)
+  zip(paste0(gsub('.*/','', f), "-ItemFolder.zip"), IN)
+}
+
+
+
+
+
+
+
+
