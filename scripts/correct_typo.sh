d874e9cdc23db4866e0fe9cbf83bd308c4bf60c6
Oliver Lindemann <lindemann09@googlemail.com>
2023-07-17 19:25:06 +0200

    clear packages

--- scripts/correct_typo.sh
+++
@@ -1,7 +0,0 @@
-# Correct using MacOS bash
-
-# Check for typo in items
-grep -r 'Interpretating' .
-
-# replace Interpretating with Interpreting
-find . -name '*.Rmd' -print0 | xargs -0 sed -i "" "s/Interpretating/Interpreting/g"
